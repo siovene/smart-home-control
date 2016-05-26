c09147a636f00e8a4a60d4386f62968606434aa0
Salvatore Iovene <salvatore.iovene@intel.com>
2016-05-26 14:30:30 +0300

    Rename app to 'Smart Home Control'

--- hooks/after_prepare/icons_and_splashscreens.js
+++ hooks/after_prepare/icons_and_splashscreens.js
@@ -20,7 +20,7 @@ var Orchestrator = require('orchestrator');
 
 var BASES = {
   android: 'res',
-  ios: 'SmarthomeDemo/Resources',
+  ios: 'SmartHomeControl/Resources',
   browser: 'www/images'
 };
 var RESOURCE_DIR = 'resources';
